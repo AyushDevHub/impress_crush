57d3b1ec38b71b79a1966db77349e66be75515be
AyushDevHub <coder.ayushshaw@gmail.com>
2024-10-11 03:00:07 +0530

    Completed

--- mobile.js
+++ mobile.js
@@ -87,3 +87,44 @@ papers.forEach(paper => {
   const p = new Paper();
   p.init(paper);
 });
+
+const typedSpan = document.getElementById("typed")
+const totype = ["Ladki", "Shivu Da", "", "Kutti"]
+
+const delayTyping_char = 150;
+const delayErasing_text = 100;
+const delayTyping_text = 1000;
+
+let totypeIndex = 0;
+let charIndex = 0;
+
+function typeText() {
+	if (charIndex < totype[totypeIndex].length) {
+		typedSpan.textContent += totype[totypeIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(typeText, delayTyping_char);
+	}
+	else {
+		setTimeout(eraseText, delayTyping_text);
+	}
+}
+
+function eraseText() {
+	if (charIndex > 0) {
+		typedSpan.textContent = totype[totypeIndex].substring(0, charIndex-1);
+		charIndex = charIndex-1;
+		setTimeout(eraseText, delayErasing_text);
+	}
+	else {
+		totypeIndex++;
+
+		if (totypeIndex >= totype.length)
+			totypeIndex = 0;
+			setTimeout(typeText, delayTyping_text);
+	}
+}
+
+window.onload = function() {
+	if (totype[totypeIndex].length) setTimeout(typeText, delayTyping_text);
+}
+
